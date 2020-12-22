d73ec62f2108d63d7e4169cff4c4bcb93bdbd8b8
nishap1225 <nishap1225@gmail.com>
2020-12-21 16:19:33 -0800

    sort of working?

--- background.js
+++ background.js
@@ -1,2 +1,9 @@
 //executed as soon as chrome is installed/refreshed
 console.log("from background")
+chrome.tabs.onActivated.addListener(tab => {
+    //console.log(tab)
+    chrome.tabs.get(tab.tabId, current_tab_info => {
+        console.log(current_tab_info.url)
+    });
+});
+//chrome.tabs.executeScript(null, {file: "./foreground.js"}, () => console.log("i injected")) // tab id you want to inject script into
